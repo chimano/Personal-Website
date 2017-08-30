ce07834228e1705e144dcd50195141f9a8118750
Chirac Manoukian <chiracmanoukian@hotmail.com>
2017-08-30 00:09:19 -0400

    Added scrolling animations

---
+++ scripts/scroll.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("a[href^='#']").click(function (e){
+        e.preventDefault();
+        var target = this.hash;
+        $("html, body").animate({
+            scrollTop: $(target).offset().top}, 500);
+
+
+ });
+});
